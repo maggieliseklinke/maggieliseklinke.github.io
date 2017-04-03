d8cb51cdfc8f8c904d86c3b07e4f753cddd305c7
mklinke <maggieklinke@gmail.com>
2017-04-03 02:36:48 -0600

    baseball

--- baseball/baseball.js
+++ baseball/baseball.js
@@ -71,7 +71,6 @@ $(document).ready(function () {
         $("#pic8").show(1000);
     });
 
-
     $("#ppic1").click(function () {
         $("#ppic1").hide(1000);
         $("#ttext1").show(1000);
@@ -139,6 +138,7 @@ $(document).ready(function () {
     });
 });
 
+
 $('.playerpic').hover(function () {
     $(this).css('cursor', 'pointer');
 });
@@ -199,6 +199,102 @@ $.getJSON("baseball.json", function (data) {
         $("#r1pos8").text("Position: " + data.Royals2014.Eight.Pos)
         $("#r1rbi8").text("RBI: " + data.Royals2014.Eight.RBI)
         $("#r1home8").text("Home Runs: " + data.Royals2014.Eight.Home)
+
+        $("#r2name1").text("Name: " + data.Royals2017.One.Name)
+        $("#r2num1").text("Jersey Number: " + data.Royals2017.One.Number)
+        $("#r2pos1").text("Position: " + data.Royals2017.One.Pos)
+        $("#r2rbi1").text("RBI: " + data.Royals2017.One.RBI)
+        $("#r2home1").text("Home Runs: " + data.Royals2017.One.Home)
+
+        $("#r2name2").text("Name: " + data.Royals2017.Two.Name)
+        $("#r2num2").text("Jersey Number: " + data.Royals2017.Two.Number)
+        $("#r2pos2").text("Position: " + data.Royals2017.Two.Pos)
+        $("#r2rbi2").text("RBI: " + data.Royals2017.Two.RBI)
+        $("#r2home2").text("Home Runs: " + data.Royals2017.Two.Home)
+
+        $("#r2name3").text("Name: " + data.Royals2017.Three.Name)
+        $("#r2num3").text("Jersey Number: " + data.Royals2017.Three.Number)
+        $("#r2pos3").text("Position: " + data.Royals2017.Three.Pos)
+        $("#r2rbi3").text("RBI: " + data.Royals2017.Three.RBI)
+        $("#r2home3").text("Home Runs: " + data.Royals2017.Three.Home)
+
+        $("#r2name4").text("Name: " + data.Royals2017.Four.Name)
+        $("#r2num4").text("Jersey Number: " + data.Royals2017.Four.Number)
+        $("#r2pos4").text("Position: " + data.Royals2017.Four.Pos)
+        $("#r2rbi4").text("RBI: " + data.Royals2017.Four.RBI)
+        $("#r2home4").text("Home Runs: " + data.Royals2017.Four.Home)
+
+        $("#r2name5").text("Name: " + data.Royals2017.Five.Name)
+        $("#r2num5").text("Jersey Number: " + data.Royals2017.Five.Number)
+        $("#r2pos5").text("Position: " + data.Royals2017.Five.Pos)
+        $("#r2rbi5").text("RBI: " + data.Royals2017.Five.RBI)
+        $("#r2home5").text("Home Runs: " + data.Royals2017.Five.Home)
+
+        $("#r2name6").text("Name: " + data.Royals2017.Six.Name)
+        $("#r2num6").text("Jersey Number: " + data.Royals2017.Six.Number)
+        $("#r2pos6").text("Position: " + data.Royals2017.Six.Pos)
+        $("#r2rbi6").text("RBI: " + data.Royals2017.Six.RBI)
+        $("#r2home6").text("Home Runs: " + data.Royals2017.Six.Home)
+
+        $("#r2name7").text("Name: " + data.Royals2017.Seven.Name)
+        $("#r2num7").text("Jersey Number: " + data.Royals2017.Seven.Number)
+        $("#r2pos7").text("Position: " + data.Royals2017.Seven.Pos)
+        $("#r2rbi7").text("RBI: " + data.Royals2017.Seven.RBI)
+        $("#r2home7").text("Home Runs: " + data.Royals2017.Seven.Home)
+
+        $("#r2name8").text("Name: " + data.Royals2017.Eight.Name)
+        $("#r2num8").text("Jersey Number: " + data.Royals2017.Eight.Number)
+        $("#r2pos8").text("Position: " + data.Royals2017.Eight.Pos)
+        $("#r2rbi8").text("RBI: " + data.Royals2017.Eight.RBI)
+        $("#r2home8").text("Home Runs: " + data.Royals2017.Eight.Home)
+
+        $("#r3name1").text("Name: " + data.Royals2015.One.Name)
+        $("#r3num1").text("Jersey Number: " + data.Royals2015.One.Number)
+        $("#r3pos1").text("Position: " + data.Royals2015.One.Pos)
+        $("#r3rbi1").text("RBI: " + data.Royals2015.One.RBI)
+        $("#r3home1").text("Home Runs: " + data.Royals2015.One.Home)
+
+        $("#r3name2").text("Name: " + data.Royals2015.Two.Name)
+        $("#r3num2").text("Jersey Number: " + data.Royals2015.Two.Number)
+        $("#r3pos2").text("Position: " + data.Royals2015.Two.Pos)
+        $("#r3rbi2").text("RBI: " + data.Royals2015.Two.RBI)
+        $("#r3home2").text("Home Runs: " + data.Royals2015.Two.Home)
+
+        $("#r3name3").text("Name: " + data.Royals2015.Three.Name)
+        $("#r3num3").text("Jersey Number: " + data.Royals2015.Three.Number)
+        $("#r3pos3").text("Position: " + data.Royals2015.Three.Pos)
+        $("#r3rbi3").text("RBI: " + data.Royals2015.Three.RBI)
+        $("#r3home3").text("Home Runs: " + data.Royals2015.Three.Home)
+
+        $("#r3name4").text("Name: " + data.Royals2015.Four.Name)
+        $("#r3num4").text("Jersey Number: " + data.Royals2015.Four.Number)
+        $("#r3pos4").text("Position: " + data.Royals2015.Four.Pos)
+        $("#r3rbi4").text("RBI: " + data.Royals2015.Four.RBI)
+        $("#r3home4").text("Home Runs: " + data.Royals2015.Four.Home)
+
+        $("#r3name5").text("Name: " + data.Royals2015.Five.Name)
+        $("#r3num5").text("Jersey Number: " + data.Royals2015.Five.Number)
+        $("#r3pos5").text("Position: " + data.Royals2015.Five.Pos)
+        $("#r3rbi5").text("RBI: " + data.Royals2015.Five.RBI)
+        $("#r3home5").text("Home Runs: " + data.Royals2015.Five.Home)
+
+        $("#r3name6").text("Name: " + data.Royals2015.Six.Name)
+        $("#r3num6").text("Jersey Number: " + data.Royals2015.Six.Number)
+        $("#r3pos6").text("Position: " + data.Royals2015.Six.Pos)
+        $("#r3rbi6").text("RBI: " + data.Royals2015.Six.RBI)
+        $("#r3home6").text("Home Runs: " + data.Royals2015.Six.Home)
+
+        $("#r3name7").text("Name: " + data.Royals2015.Seven.Name)
+        $("#r3num7").text("Jersey Number: " + data.Royals2015.Seven.Number)
+        $("#r3pos7").text("Position: " + data.Royals2015.Seven.Pos)
+        $("#r3rbi7").text("RBI: " + data.Royals2015.Seven.RBI)
+        $("#r3home7").text("Home Runs: " + data.Royals2015.Seven.Home)
+
+        $("#r3name8").text("Name: " + data.Royals2015.Eight.Name)
+        $("#r3num8").text("Jersey Number: " + data.Royals2015.Eight.Number)
+        $("#r3pos8").text("Position: " + data.Royals2015.Eight.Pos)
+        $("#r3rbi8").text("RBI: " + data.Royals2015.Eight.RBI)
+        $("#r3home8").text("Home Runs: " + data.Royals2015.Eight.Home)
     })
     .fail(function (jqxhr, status, error) {
         console.log('error', status, error)
@@ -206,24 +302,5 @@ $.getJSON("baseball.json", function (data) {
 
 $(function () {
     $("#insert").load("header.html");
-});
-
-
-$("#page-nav").on("click", "a", function (evt) {
-    evt.preventDefault();
-    console.log('click occured');
-    // With the text value get the needed value from the weather.json file
-    var team = $(this).text(); // Franklin, etc...
-    console.log(jsonCity);
-    $.ajax({
-        url: "https://blainerobertson.github.io/230d2l/js/weather.json",
-        dataType: "json",
-        success: function (data) {
-            console.log(data);
-            console.log(data[jsonCity]);
-            var zip = data[jsonCity].zip;
-            console.log(zip);
-            getData(zip);
-        }
-    });
+    $("#inserth").load("footer.html");
 });
